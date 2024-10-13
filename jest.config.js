48a987f5dd16c9ec2fb8686b8ea1f7bc407ebafa
mch <45938634+mincheolhong@users.noreply.github.com>
2024-10-14 02:48:14 +0900

    [chore] jest 경로 옵션 수정

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^baseball/(.*)$': '<rootDir>/baseball/$1',
+  },
 };
